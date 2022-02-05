9a716c2f2a35bd951929913169d33224374db595
Michelle-2104 <chidowemichelle@gmail.com>
2022-02-05 18:51:45 +0200

    Edit rps.js

--- rps.js
+++ rps.js
@@ -1,7 +1,9 @@
-function computerPlay() {
+const computerSelectionDisplay = document.getElementById('computerSelection')
+const playerSelectionDisplay = document.getElementById('playerSelection')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectionAll('button')
 
-
-
-rock && sciccors = win
-alert rock beats scissors
-}
+possibleChoices.forEach(possibleChoice => possibleChoices.addEventListener('click'(e) => {
+ userChoice = e.target.id
+ playerSelectionDisplay.innerHTML
+}))
